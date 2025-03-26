b30fbc39113d62daecdf2640f43d6d3f9ef960f8
Tayrtahn <tayrtahn@gmail.com>
2025-03-26 13:42:23 -0400

    EntityCoordinates.WithEntityId -> SharedTransformSystem.WithEntityId

--- Content.Shared/Magic/SharedMagicSystem.cs
+++ Content.Shared/Magic/SharedMagicSystem.cs
@@ -279,7 +279,7 @@ public abstract class SharedMagicSystem : EntitySystem
         // If applicable, this ensures the projectile is parented to grid on spawn, instead of the map.
         var fromMap = _transform.ToMapCoordinates(fromCoords);
         var spawnCoords = _mapManager.TryFindGridAt(fromMap, out var gridUid, out _)
-            ? fromCoords.WithEntityId(gridUid, EntityManager)
+            ? _transform.WithEntityId(fromCoords, gridUid)
             : new(_mapManager.GetMapEntityId(fromMap.MapId), fromMap.Position);
 
         var ent = Spawn(ev.Prototype, spawnCoords);
